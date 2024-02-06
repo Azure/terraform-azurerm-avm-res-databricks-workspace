48ac02cc9370a9bc5738ff7f77a5af319ce8e2ea
acauret <andrew.auret@alikeconsultancy.co.uk>
2024-02-06 18:03:17 +0000

    include missing variables and update to include resource group

--- locals.tf
+++ locals.tf
@@ -1,4 +1,5 @@
 locals {
+  resource_group_location            = try(data.azurerm_resource_group.parent[0].location, null)
   role_definition_resource_substring = "/providers/Microsoft.Authorization/roleDefinitions"
 }
 
