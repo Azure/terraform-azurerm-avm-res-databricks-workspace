bf4de97d3cd544d6466861be75f3ccfda06c73ee
acauret <andrew.auret@alikeconsultancy.co.uk>
2024-02-14 12:47:35 +0000

    fix linting issues in examples

--- examples/customer-managed-key/main.tf
+++ examples/customer-managed-key/main.tf
@@ -1,6 +1,10 @@
 terraform {
   required_version = ">= 1.0.0"
   required_providers {
+    azuread = {
+      source  = "hashicorp/azuread"
+      version = ">= 2.15.0"
+    }
     azurerm = {
       source  = "hashicorp/azurerm"
       version = ">= 3.7.0, < 4.0.0"
