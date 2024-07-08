5430615d3a3846baab42b2207926f569ede102b3
Sebastian Graef <segraef@microsoft.com>
2024-07-08 16:24:01 +1000

    feat: Update azurerm_management_lock count logic in main.tf

--- main.tf
+++ main.tf
@@ -37,14 +37,14 @@ resource "azurerm_databricks_workspace" "this" {
   }
 }
 
-resource "azurerm_management_lock" "this" {
-  count = var.lock != null ? 1 : 0
+  resource "azurerm_management_lock" "this" {
+    count = var.lock != null ? 1 : 0
 
-  lock_level = var.lock.kind
-  name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
-  scope      = azurerm_MY_RESOURCE.this.id
-  notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete the resource or its child resources." : "Cannot delete or modify the resource or its child resources."
-}
+    lock_level = var.lock.kind
+    name       = coalesce(var.lock.name, "lock-${var.lock.kind}")
+    scope      = azurerm_databricks_workspace.this.id
+    notes      = var.lock.kind == "CanNotDelete" ? "Cannot delete the resource or its child resources." : "Cannot delete or modify the resource or its child resources."
+  }
 
 resource "azurerm_role_assignment" "this" {
   for_each = var.role_assignments
