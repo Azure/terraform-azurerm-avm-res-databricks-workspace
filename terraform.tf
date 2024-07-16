033b6dd5faaa73c045237420be1e514fa3a1dd59
Bilal Amjad <79409563+mbilalamjad@users.noreply.github.com>
2024-07-16 00:35:56 -0700

    chore: repository governance (#56)
    * fix: grept apply
    
    * chore: Update modtm dependency to version 0.3
    
    * chore: Update modtm dependency to version 0.3
    
    * chore: Update modtm dependency to version 0.3
    
    * chore: Update random dependency to version 3.5.0
    
    ---------
    
    Co-authored-by: github-actions[bot] <github-actions[bot]@users.noreply.github.com>
    Co-authored-by: Sebastian Graef <segraef@microsoft.com>

--- terraform.tf
+++ terraform.tf
@@ -6,6 +6,10 @@ terraform {
       source  = "hashicorp/azurerm"
       version = ">= 3.71.0"
     }
+    modtm = {
+      source  = "Azure/modtm"
+      version = "~> 0.3"
+    }
     random = {
       source  = "hashicorp/random"
       version = ">= 3.5.0"
